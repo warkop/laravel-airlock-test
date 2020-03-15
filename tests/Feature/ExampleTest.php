156e09d8fd13d6073af9596bbec3c8a6f5347dde
Mohammad Khafid Barokum <navigator619@gmail.com>
2020-03-15 13:26:58 +0700

    test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,7 +2,9 @@
 
 namespace Tests\Feature;
 
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Laravel\Airlock\Airlock;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
@@ -17,5 +19,17 @@ class ExampleTest extends TestCase
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+
+    public function test_task_list_can_be_retrieved()
+    {
+        Airlock::actingAs(
+            factory(User::class)->create(),
+            ['*']
+        );
+
+        $response = $this->get('/api/user');
+
+        $response->assertOk();
     }
 }
